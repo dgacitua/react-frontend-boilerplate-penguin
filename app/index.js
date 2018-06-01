ad5bb71cc87ca3bfcd7e3b98313392a620aa17a7
Daniel Gacitua <daniel.gacitua@usach.cl>
2018-06-01 16:54:15 -0400

    First commit

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactBootstrap from 'react-bootstrap';
+
+import logo from '../assets/react-logo.svg';
+import './index.css';
+
+const App = () => (
+  <div className="App">
+    <img className="App-Logo" src={logo} alt="React Logo" />
+    <h1 className="App-Title">React Boilerplate "Penguin Edition"</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
